984d1bb7a4ec7f556321e52d27235f88864aed1a
Ammar Ladhani <55207849+ammarladhani@users.noreply.github.com>
2024-05-20 00:35:41 -0500

    Made cursor blink

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
 
 const textArray = ["Software Engineer", "Tech Enthusiast", "Open Source Contributor", "Eagle Scout", "Coffee Lover", "Gamer", "Fitness Addict", "Traveler", "Lifelong Learner", "Volunteer", "Human"];
 const typingDelay = 160;
@@ -9,12 +10,14 @@ let charIndex = 0;
 
 function type() {
     if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
         charIndex++;
         //generate a number between -40 and 40
         let randomNumber = Math.floor(Math.random() * 80) - 40;
         setTimeout(type, typingDelay + randomNumber);
     } else {
+        cursorSpan.classList.remove("typing");
         let randomNumber = Math.floor(Math.random() * 800) - 400;
         setTimeout(erase, newTextDelay+randomNumber);
     }
@@ -22,18 +25,20 @@ function type() {
 
 function erase() {
     if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
         typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
         charIndex--;
         let randomNumber = Math.floor(Math.random() * 40) - 20;
         setTimeout(erase, erasingDelay + randomNumber);
     } else {
+        cursorSpan.classList.remove("typing");
         textArrayIndex++;
         if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-        let randomNumber = Math.floor(Math.random() * 800) - 400;
-        setTimeout(type, typingDelay + 1100 + randomNumber);
+        let randomNumber = Math.floor(Math.random() * 440) - 220;
+        setTimeout(type, typingDelay + 880 + randomNumber);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(type, newTextDelay + 250);
+    if (textArray.length) setTimeout(type, newTextDelay + 200);
 });
